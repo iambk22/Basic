faaa111c6fcded7069237748234d137b694caf0b
iambk22 <bharathikrishna22@gmail.com>
2022-05-16 19:05:49 +0530

    Basic

---
+++ src/org/basic/BasicClass.java
@@ -0,0 +1,26 @@
+package org.basic;
+
+public class BasicClass {
+	public void add() {
+		System.out.println("Add");
+
+	}
+	public void sub() {
+		System.out.println("Sub");
+
+	}
+	public void mul() {
+		System.out.println("Mul");
+
+	}
+	
+	public static void main(String[] args) {
+		BasicClass b = new BasicClass();
+		b.add();
+		b.sub();
+		b.mul();
+	}
+	
+	
+
+}
